fa1a7c14acfe9990d45344ff6bcf333363e6fa0b
alejandro <a.lucena86@gmail.com>
2022-03-19 10:58:00 +0000

    bg dinamico

--- components/FondoDinamico.js
+++ components/FondoDinamico.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import fondo from '../public/assets/images/fondo.png'
 
-import columna_left from '../public/assets/images/columna_left.png'
-import columna_right from '../public/assets/images/columna_right.png'
+import columna_derecha from '../public/assets/images/columna_derecha.png'
+import columna_izquierda from '../public/assets/images/columna_izquierda.png'
 import monje from '../public/assets/images/monje.png'
 import luz from '../public/assets/images/luz.png'
 import luz_rayos from '../public/assets/images/luz_rayos.png'
@@ -15,41 +15,47 @@ const FondoDinamico = ( props ) => {
     }
 
     const [columnLeftPosition, setColumnLeftPosition ] = useState({ bottom: 0, left: 0})
-    const [columnRightPosition, setColumnRightPosition ] = useState({ bottom: 0, right: 0})
-    const [monkPosition, setMonkPosition ] = useState({ top: -50 , left: 0})
-    const [luzPosition, setLuzPosition ] = useState({ top: 0 , left: 400})
-    const [luzRayosPosition, setLuzRayosPosition ] = useState({ top: 0 , left: 0})
+    const [columnRightPosition, setColumnRightPosition ] = useState({ bottom: 0, left: 0})
+    const [monkPosition, setMonkPosition ] = useState({ bottom: 0 , left: 0})
+    const [luzPosition, setLuzPosition ] = useState({ bottom: 0 , left: 0})
+    const [luzRayosPosition, setLuzRayosPosition ] = useState({ bottom: 0 , left: 0})
 
     const columnLeftStyle = {
+        backgroundImage: `url('${columna_izquierda.src}')`,
         bottom: columnLeftPosition.bottom,
         left: columnLeftPosition.left
     }
 
     const columnRightStyle = {
+        backgroundImage: `url('${columna_derecha.src}')`,
         bottom: columnRightPosition.bottom,
-        right: 0 + columnRightPosition.right,
+        left: columnRightPosition.left
     }
 
     const monkStyle = {
-        top: -50 + monkPosition.top,
-        left: 0 + monkPosition.left,
+        backgroundImage: `url('${monje.src}')`,
+        bottom: monkPosition.bottom,
+        left: monkPosition.left
     }
 
     const lightStyle = {
-        top: luzPosition.top,
-        left: 400 + luzPosition.left,
+        backgroundImage: `url('${luz.src}')`,
+        bottom: luzPosition.bottom,
+        left: luzPosition.left
     }
 
     const lighRaytStyle = {
-        top: luzRayosPosition.top,
+        backgroundImage: `url('${luz_rayos.src}')`,
+        bottom: luzRayosPosition.bottom,
         left: luzRayosPosition.left,
     }
 
     const mouseMoveHandler = (ev) => {
-        setColumnLeftPosition({bottom: -ev.screenY/300, left: -ev.screenX/300})
-        setColumnRightPosition({bottom: -ev.screenY/50, right: ev.screenX/50})
-        setMonkPosition({top: ev.screenY/75, left: -ev.screenX/75})
-        setLuzPosition({top: ev.screenY/300, left: -ev.screenX/300})
+        setColumnLeftPosition({bottom: ev.screenY/300, left: -ev.screenX/300})
+        setColumnRightPosition({bottom: ev.screenY/300, left: -ev.screenX/300})
+        setMonkPosition({bottom: ev.screenY/100, left: -ev.screenX/100})
+        setLuzPosition({bottom: ev.screenY/40, left: -ev.screenX/40})
+        setLuzRayosPosition({bottom: ev.screenY/50, left: -ev.screenX/50})
     }
 
     useEffect(() => {
@@ -58,11 +64,11 @@ const FondoDinamico = ( props ) => {
     
     return (
         <div data-component="FondoDinamico" className='h-screen w-screen relative' style={style}>
-            <img  src={luz.src} alt="Columna Ilustration" className="absolute scale-150 opacity-30" style={lightStyle} />
-            <img  src={luz_rayos.src} alt="Columna Ilustration" className="absolute opacity-30" style={lighRaytStyle} />
-            <img  src={monje.src} alt="Columna Ilustration" className="absolute scale-125" style={monkStyle} />
-            <img  src={columna_left.src} alt="Columna Ilustration" className="absolute" style={columnLeftStyle} />
-            <img  src={columna_right.src} alt="Columna Ilustration" className="absolute" style={columnRightStyle} />
+            <div className='absolute h-screen w-screen bg-cover bg-no-repeat' style={lightStyle}></div>
+            <div className='absolute h-screen w-screen bg-cover bg-no-repeat' style={lighRaytStyle}></div>
+            <div className='absolute h-screen w-screen bg-cover bg-no-repeat' style={monkStyle}></div>
+            <div className='absolute h-screen w-screen bg-cover bg-no-repeat ' style={columnLeftStyle}></div>
+            <div className='absolute h-screen w-screen bg-cover bg-no-repeat' style={columnRightStyle}></div>
         </div>
     )
 }
