c3654c0e9b07caa0b2c52e564dff2849038f5b44
alejandro <a.lucena86@gmail.com>
2022-06-23 16:40:02 +0100

    working on video

--- components/FullVideo.js
+++ components/FullVideo.js
@@ -5,21 +5,27 @@ const Galeria = ( props ) => {
     const [showVideo, setShowVideo] = useState(false)
 
     return (
-        <div data-component='FullVideo' className='w-full flex pt-20 pb-28 justify-center'>
+        <div data-component='FullVideo' className='w-full flex pt-14 sm:pt-20 pb-28 justify-center'>
             <div className='w-full lg:max-w-6xl px-5 xl:px-2'>
-
+                <div className='sm:hidden text-3xl text-white font-cheddar text-center mb-4'>
+                    <p>Video</p>
+                </div>
                 <div className={`flex w-full justify-center cursor-pointer relative group rounded-lg transition-all duration-300 bg-black bg-opacity-0 ${showVideo ? "shadow-video-hover" : "hover:bg-opacity-80 hover:shadow-video-hover"}`} onClick={()=>setShowVideo(true)}>
                     {!showVideo &&
-                        <div className="absolute inset-1/2 h-20 w-20 rounded-full bg-white bg-opacity-30 transform -translate-x-2/4 -translate-y-2/4 group-hover:bg-opacity-80 transition-all duration-300 z-10 flex items-center justify-center shadow-md">
+                        <>
+                        <div className="hidden sm:flex absolute inset-1/2 h-20 w-20 rounded-full bg-white bg-opacity-30 transform -translate-x-2/4 -translate-y-2/4 group-hover:bg-opacity-80 transition-all duration-300 z-10  items-center justify-center shadow-md">
                             <div className="text-yellow text-3xl transform translate-x-0.5 translate-y-0.5">▶</div>
                         </div>
+                        <div className="sm:hidden absolute inset-1/2 h-20 w-20 rounded-full shadow-image-hover transform -translate-x-2/4 -translate-y-2/4 bg-black transition-all duration-300 z-10 flex items-center justify-center shadow-md">
+                            <div className="text-yellow text-3xl transform translate-x-0.5 translate-y-0.5">▶</div>
+                        </div>
+                        </>
                     }
                     {showVideo 
                         ? 
                         <iframe className="w-full" height="630" src="https://www.youtube.com/embed/pwEe5Z2MSB0?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>   
                         :
-                        // <div className="w-full aspect-w-16 aspect-h-9 bg-no-repeat bg-cover bg-center" style={style}></div>
-                        <div className="text-white group-hover:bg-black/10 w-[1020px] h-[630px] font-cheddar text-[20rem] leading-none text-center"> 
+                        <div className="text-white group-hover:bg-black/10 w-full lg:w-[1020px] lg:h-[630px] font-cheddar text-9xl sm:text-[14rem] lg:text-[20rem] leading-none text-center"> 
                             <p className="text-yellow">AFUERA,</p>
                             <p>fLINK<span className="text-yellow">S</span></p>
                         </div>
--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
         <HeaderNav/>
         <Intro/>
         <Conciertos/>
-        {/* <FullVideo/> */}
+        <FullVideo/>
         {/* <SobreNosotros/> */}
         {/* <Galeria/> */}
         {/* <Contacto/> */}
