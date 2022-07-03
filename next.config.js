53e96f39b4f44c9123440888b4240f95b0e6e6f3
alejandro <a.lucena86@gmail.com>
2022-07-03 22:26:38 +0100

    Update nest config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+module.exports = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
