92455b64601da35f597396fbc7b715fe9a173a52
alejandro <a.lucena86@gmail.com>
2022-10-03 21:07:20 +0100

    testing arrows 2

--- components/slider/review_arrow.js
+++ components/slider/review_arrow.js
@@ -8,7 +8,7 @@ const ReviewArrow = (props) => {
 
     return (
         <div className={`font-cheddar rounded-full w-16 h-16 bg-black absolute -bottom-10 bg-opacity-40 hover:bg-opacity-80 transition-all text-white items-center justify-center text-5xl cursor-pointer hidden sm:flex ${props.direction === "prev" ? "left-12 z-20" : "right-12"}`} onClick={props.onClick}>
-            <p className="mb-2">{arrow}</p>
+            <p className="mb-1">{arrow}</p>
         </div>
     )
 }
