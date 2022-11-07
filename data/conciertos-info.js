cc2b0c6e744cfed125587fd6b560d1a0e873d4e4
alejandro <a.lucena86@gmail.com>
2022-11-07 21:09:40 +0000

    update comands

--- data/conciertos-info.js
+++ data/conciertos-info.js
@@ -8,25 +8,6 @@ const conciertos = [
         sala: "Gruta 77",
         entradas_link: "https://movingtickets.com/Entradas-Postales-Negras-Flinks-en-concierto-Gruta-77-Madrid/"
     },
-    {
-        dia: "10",
-        mes: "DIC",
-        ano: "2022",
-        ciudad: "Madrid",
-        lugar: "Sala",
-        sala: "Tu Cara",
-        entradas_link: "https://movingtickets.com/Entradas-Postales-Negras-Flinks-en-concierto-Gruta-77-Madrid/"
-    },
-    {
-        dia: "23",
-        mes: "ENE",
-        ano: "2023",
-        ciudad: "Madrid",
-        lugar: "Sala",
-        sala: "Gruta 77",
-        entradas_link: "https://movingtickets.com/Entradas-Postales-Negras-Flinks-en-concierto-Gruta-77-Madrid/"
-    },
-
 ]
 
 export default conciertos
